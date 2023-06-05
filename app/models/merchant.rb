0a0c7bb54e680ccc5102df9677207ec4f90fcb83
Jawad <creatorjawad@gmail.com>
2023-06-05 22:40:06 +0500

    Can't delete the merchant, if he has transactions

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,6 @@
 class Merchant < ApplicationRecord
+  before_destroy :check_for_transactions
+
   default_scope { order(created_at: :desc) }
 
   has_many :transactions
@@ -6,4 +8,12 @@ class Merchant < ApplicationRecord
   validates :email,
             uniqueness: true,
             format: { with: EMAIL_REGEX}
+
+  private
+
+  def check_for_transactions
+    if transactions.any?
+      raise "Can't delete, this merchant has transactions"
+    end
+  end
 end
